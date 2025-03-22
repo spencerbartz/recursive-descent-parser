32aed0bc2db218ffb9b1e7cf08f1bd0cbb2aac1d
Spencer Bartz <spencerbartz@gmail.com>
2025-03-21 18:00:36 -0700

    PLI-8 Trying to use setInterval to fake blocking

--- src/InputStream.js
+++ src/InputStream.js
@@ -10,10 +10,36 @@ class InputStream {
         return this.#inputPointer;
     }
 
-    requestInput() {
+    blockingPrompt(message) {
+        //return new Promise(resolve => {
+        const intervalId = setInterval(async () => {
+                const input = await Prompts.prompt(message); // <- Problem: This does not block nor does it simulate blocking behavior
+                
+                if (input) {
+                    debugger;
+                    clearInterval(intervalId);
+                    return input;
+                }
+            }, 1000);
+        //});
+    }
+    
+    async runPrompt() {
+        const userInput = await this.blockingPrompt('Enter a value');
+        console.log("User input:", userInput);
+        return userInput;
+    }
+
+
+    async requestInput() {
         let input = undefined;
         if (this.streamSource.type === 'user') {
-            input = prompt('Enter a value');
+            // Prompts.prompt('test-prompt').then(x => {
+            //     debugger;
+            // });
+            // input = this.runPrompt(); 
+
+            input = this.blockingPrompt('Enter a value');
         } else if (this.streamSource.type === 'UI') {
             input = this.streamSource.value.charAt(this.#inputPointer);
             this.#inputPointer++;
